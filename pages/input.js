4e3d7f803a66594112117ef313b0ba8fb383a05e
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-10-15 09:06:48 +0700

    Finishing Form Input View

--- pages/input.js
+++ pages/input.js
@@ -1,7 +1,9 @@
 const Input = () => {
     return (
-        <div>
-            This Is Input Page
+        <div className="text-center">
+            <label for="message" className="text-xl block mb-3 font-medium text-gray-900">Type The Receipt Number</label>
+            <textarea id="message" className="block p-2.5 min-w-250 min-h-250 lg:min-w-750 lg:min-h-500 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+            <button type="submit" className="p-3 bg-blue-400 rounded-md mt-5 text-white min-w-[80px]">Anu</button>
         </div>
     );
 }
